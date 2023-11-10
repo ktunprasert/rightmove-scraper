769a70bd5524bd2b4633ea4e57ee016201334a9c
ktunprasert <ktunprasert@outlook.com>
2023-11-10 21:14:50 +0000

    init

---
+++ lib/rightmove_scraper.ex
@@ -0,0 +1,18 @@
+defmodule RightmoveScraper do
+  @moduledoc """
+  Documentation for `RightmoveScraper`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> RightmoveScraper.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/rightmove_scraper_test.exs
@@ -0,0 +1,8 @@
+defmodule RightmoveScraperTest do
+  use ExUnit.Case
+  doctest RightmoveScraper
+
+  test "greets the world" do
+    assert RightmoveScraper.hello() == :world
+  end
+end
