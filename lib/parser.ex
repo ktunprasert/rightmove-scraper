d15639c9c1d2d8f3f7d22dd4814613d84b0b2245
ktunprasert <ktunprasert@outlook.com>
2023-11-10 22:11:02 +0000

    parser: get result counter

--- lib/parser.ex
+++ lib/parser.ex
@@ -1,4 +1,13 @@
 defmodule Rightmove.Parser do
+  def get_total_results(html) do
+    html
+    |> Floki.find(".searchHeader-resultCount")
+    |> Floki.text()
+    |> String.trim()
+    |> String.replace(",", "")
+    |> String.to_integer()
+  end
+
   def find_properties(html) do
     for card <- html |> Floki.find(".propertyCard") do
       %{
@@ -10,8 +19,19 @@ defmodule Rightmove.Parser do
           |> Floki.find(".propertyCard-link")
           |> Floki.attribute("href")
           |> List.first()
-          |> then(&"https://www.rightmove.co.uk#{&1}")
+          |> then(&"https://www.rightmove.co.uk#{&1}"),
+        "agency" =>
+          card
+          |> Floki.find(".propertyCard-branchLogo-image")
+          |> Floki.attribute("alt")
+          |> List.first(),
+        "floorplan" => card |> Floki.find("a[title=\"Floor plan\"]") != []
       }
     end
   end
 end
+
+# https://www.rightmove.co.uk/property-to-rent/find.html?locationIdentifier=REGION%5E85362&maxBedrooms=2&minBedrooms=2&maxPrice=2800&propertyTypes=&includeLetAgreed=false&mustHave=&dontShow=&furnishTypes=furnished&keywords=
+# https://www.rightmove.co.uk/property-to-rent/find.html?locationIdentifier=REGION^85362&maxBedrooms=2&minBedrooms=2&maxPrice=2800&index=24&propertyTypes=&includeLetAgreed=false&mustHave=&dontShow=&furnishTypes=furnished&keywords%3d
+
+# https://www.rightmove.co.uk/property-to-rent/find.html?locationIdentifier=REGION^85362&maxBedrooms=2&minBedrooms=2&maxPrice=2800&index=24&propertyTypes=&includeLetAgreed=false&mustHave=&dontShow=&furnishTypes=furnished&keywords=
