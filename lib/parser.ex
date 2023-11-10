f2a38d8a5b003b4f5c26da14b064d8d9681b2482
ktunprasert <ktunprasert@outlook.com>
2023-11-10 21:43:58 +0000

    parser: init

---
+++ lib/parser.ex
@@ -0,0 +1,16 @@
+defmodule Rightmove.Parser do
+  def find_properties(html) do
+    for card <- html |> Floki.find(".propertyCard") do
+      %{
+        "title" => card |> Floki.find(".propertyCard-title") |> Floki.text() |> String.trim(),
+        "address" => card |> Floki.find(".propertyCard-address") |> Floki.text(),
+        "price" => card |> Floki.find(".propertyCard-priceValue") |> Floki.text(),
+        "link" =>
+          card
+          |> Floki.find(".propertyCard-link")
+          |> Floki.attribute("href")
+          |> Enum.map(&"https://www.rightmove.co.uk#{&1}")
+      }
+    end
+  end
+end
