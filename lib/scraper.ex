6d124dd58fffb3fec0859d17326744f17f0f813d
ktunprasert <ktunprasert@outlook.com>
2023-11-10 22:10:45 +0000

    scraper: async parsing all pages

--- lib/scraper.ex
+++ lib/scraper.ex
@@ -4,8 +4,23 @@ defmodule Rightmove.Scraper do
   def scrape_links(url) do
     {:ok, %{body: body}} = url |> HTTPoison.get()
 
-    body
-    |> Floki.parse_document!()
-    |> Parser.find_properties()
+    html =
+      body
+      |> Floki.parse_document!()
+
+    results = Parser.get_total_results(html)
+
+    htmls =
+      [html] ++
+        Enum.map(0..results//24, fn i ->
+          url = url <> "&index=#{i}"
+
+          url |> HTTPoison.get!() |> Map.get(:body) |> Floki.parse_document!()
+        end)
+
+    Task.async_stream(htmls, fn html ->
+      Parser.find_properties(html)
+    end)
+    |> Enum.flat_map(fn {:ok, properties} -> properties end)
   end
 end
