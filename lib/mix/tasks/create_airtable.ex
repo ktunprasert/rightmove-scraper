117834054911c3951b4f2b20b452f445214759b6
ktunprasert <ktunprasert@outlook.com>
2023-11-12 14:32:46 +0000

    task.create_airtable: upserting records from json

---
+++ lib/mix/tasks/create_airtable.ex
@@ -0,0 +1,29 @@
+defmodule Mix.Tasks.Create.Airtable do
+  use Mix.Task
+
+  alias Rightmove.{Property, Airtable}
+
+  @impl Mix.Task
+  def run(_args) do
+    Mix.Task.run("app.start")
+
+    IO.puts("Upserting records...")
+
+    # Read from gen/properties.json
+    properties = File.read!("gen/properties.json") |> Jason.decode!(keys: :atoms)
+
+    records_payload =
+      properties
+      |> Enum.map(fn p ->
+        p |> Property.from_map() |> Property.to_airtable_payload() |> then(&%{"fields" => &1})
+      end)
+
+    records_payload
+    |> Stream.chunk_every(10)
+    |> Task.async_stream(&Airtable.create_records/1)
+    |> Enum.to_list()
+    |> IO.inspect()
+
+    IO.puts("Done!")
+  end
+end
