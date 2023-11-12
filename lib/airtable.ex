57bcbc20fe7dd467cd5ec6f85802f6de046069a9
ktunprasert <ktunprasert@outlook.com>
2023-11-12 15:00:59 +0000

    airtable: read from env

--- lib/airtable.ex
+++ lib/airtable.ex
@@ -3,7 +3,7 @@ defmodule Rightmove.Airtable do
   @table_id "tblZ7NbRj2KIUFa7V"
 
   @url "https://api.airtable.com/v0/#{@app_id}/#{@table_id}/"
-  @auth_token Application.compile_env!(:rightmove_scraper, :auth_token)
+  @auth_token System.get_env("TOKEN")
 
   def create_record(payload) do
     %{status_code: status, body: body} =
