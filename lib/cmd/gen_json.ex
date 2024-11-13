c401c6640e63614cf77ce4e9136ccba9d92ffe47
ktunprasert <ktunprasert@outlook.com>
2024-11-13 10:04:22 +0000

    task.gen_json: why were we scraping twice??

--- lib/cmd/gen_json.ex
+++ lib/cmd/gen_json.ex
@@ -6,6 +6,7 @@ defmodule Mix.Tasks.Gen.Json do
   alias Rightmove.Scraper
 
   @url "https://www.rightmove.co.uk/property-to-rent/find.html?locationIdentifier=REGION^85362&maxBedrooms=2&minBedrooms=4&maxPrice=3300&propertyTypes&includeLetAgreed=false&mustHave&dontShow&furnishTypes=furnished&keywords"
+  @api "https://www.rightmove.co.uk/api/property-search/listing/search?locationIdentifier=REGION%5E85362&maxBedrooms=2&minBedrooms=4&maxPrice=3300&propertyTypes=&includeLetAgreed=false&mustHave=&dontShow=&furnishTypes=furnished&keywords=&sortType=6&channel=RENT&transactionType=LETTING"
 
   @impl Mix.Task
   def run(_args) do
@@ -13,13 +14,11 @@ defmodule Mix.Tasks.Gen.Json do
 
     IO.puts("Beginning to scrape")
 
-    Scraper.scrape_links(@url)
-
     filename = File.cwd!() <> ~s(/gen/properties.json)
 
     {:ok, file} = File.open(filename, [:write])
 
-    IO.binwrite(file, Scraper.scrape_links(@url) |> Jason.encode!())
+    IO.binwrite(file, Scraper.scrape_links(@url, @api) |> Jason.encode!())
 
     IO.puts("Finished scraping wrote to #{filename}")
   end
